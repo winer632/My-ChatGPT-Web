5ede3ab73e0fd105d18195e38ae001de038f8819
wangxinxin <wangxinxin@sensetime.com>
2023-06-01 14:02:44 +0800

    init

---
+++ app/layout.tsx
@@ -0,0 +1,46 @@
+/* eslint-disable @next/next/no-page-custom-font */
+import "./styles/globals.scss";
+import "./styles/markdown.scss";
+import "./styles/highlight.scss";
+import { getBuildConfig } from "./config/build";
+import { Analytics } from "@vercel/analytics/react";
+
+const buildConfig = getBuildConfig();
+
+export const metadata = {
+  title: "My ChatGPT Web",
+  description: "Your personal ChatGPT Chat Bot.",
+  viewport: {
+    width: "device-width",
+    initialScale: 1,
+    maximumScale: 1,
+  },
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "#fafafa" },
+    { media: "(prefers-color-scheme: dark)", color: "#151515" },
+  ],
+  appleWebApp: {
+    title: "My ChatGPT Web",
+    statusBarStyle: "default",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <meta name="version" content={buildConfig.commitId} />
+        <link rel="manifest" href="/site.webmanifest"></link>
+        <script src="/serviceWorkerRegister.js" defer></script>
+      </head>
+      <body>
+        {children}
+        <Analytics />
+      </body>
+    </html>
+  );
+}
