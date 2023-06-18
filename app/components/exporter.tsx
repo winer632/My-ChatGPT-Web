500c8a3809cecdd3cd4c9c159b151cb71739d028
wangxinxin <winer632@qq.com>
2023-06-18 20:04:22 +0800

    modify url

--- app/components/exporter.tsx
+++ app/components/exporter.tsx
@@ -438,9 +438,7 @@ export function ImagePreviewer(props: {
 
           <div>
             <div className={styles["main-title"]}>ChatGPT China</div>
-            <div className={styles["sub-title"]}>
-              github.com/openai/My-ChatGPT
-            </div>
+            <div className={styles["sub-title"]}>bizoe.tech</div>
             <div className={styles["icons"]}>
               <ExportAvatar avatar={config.avatar} />
               <span className={styles["icon-space"]}>&</span>
